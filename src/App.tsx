b683c7aa70af1981f7174a715c8d3e10b433ae15
Elias Sartori <elias.sartori@gmail.com>
2021-03-17 19:48:36 -0300

    make it better

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Index from "./containers/Index";
+import New from "./containers/New";
+import Edit from "./containers/Edit";
+import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Index} />
+      <Route exact path="/new" component={New} />
+      <Route exact path="/edit/:id" component={Edit} />
+    </Switch>
+  </BrowserRouter>
+);
 
 export default App;
